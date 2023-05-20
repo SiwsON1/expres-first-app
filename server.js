e803fd3a91f74e7be3ba60af56865292404a0fdc
Marcin Siwonia <mahinek12@gmail.com>
2023-05-20 17:08:05 +0200

    Add img input

--- server.js
+++ server.js
@@ -1,42 +1,58 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
 const app = express();
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
-  app.get('/', (req, res) => {
-    res.render('index');
-  });
-  
-  app.get('/about', (req, res) => {
-    res.render('about.hbs', { layout: 'dark' });
-  });
-  
-  app.get('/contact', (req, res) => {
-    res.render('contact');
-  });
-  
-  app.get('/info', (req, res) => {
-    res.render('info');
-  });
-  
-  app.get('/history', (req, res) => {
-    res.render('history');
-  });
+const upload = multer({ dest: 'uploads/' });
 
-  app.get('/hello/:name', (req, res) => {
-    res.render('hello', { name: req.params.name });
-  });
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-  
+app.get('/about', (req, res) => {
+  res.render('about.hbs', { layout: 'dark' });
+});
 
-  app.use((req, res) => {
-    res.status(404).send('404 not found...');
-  });
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
+
+app.post('/contact/send-message', upload.single('file'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message) {
+    if (req.file) {
+      res.render('contact', { isSent: true, fileName: req.file.originalname });
+    } else {
+      res.render('contact', { isSent: true });
+    }
+  } else {
+    res.render('contact', { isError: true });
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+});
 
 app.listen(9000, () => {
   console.log('Server is running on port: 9000');
